c53a443d5f89c3e5a6776700c31ae4a80b22530d
Balaji <60376123+Balaji0112@users.noreply.github.com>
2024-01-01 21:50:55 -0800

    Time: 172 ms (21.92%), Space: 19.8 MB (14.47%) - LeetHub

--- 0136-single-number/0136-single-number.py
+++ 0136-single-number/0136-single-number.py
@@ -1,8 +1,6 @@
 class Solution:
     def singleNumber(self, nums: List[int]) -> int:
         c = nums[0]
-        if(len(nums) == 1):
-            return(c)
         for i in range(1, len(nums)):
             c = (c ^ nums[i])
         return(c)

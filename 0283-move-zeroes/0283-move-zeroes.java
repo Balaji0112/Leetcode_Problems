b284bdc0d9d3db53ba50e9aefcefe0355b172524
Balaji <60376123+Balaji0112@users.noreply.github.com>
2024-01-09 13:54:02 -0800

    Time: 3 ms (25.52%), Space: 45.9 MB (27.37%) - LeetHub

---
+++ 0283-move-zeroes/0283-move-zeroes.java
@@ -0,0 +1,24 @@
+class Solution {
+    public void moveZeroes(int[] arr) {
+        int ptr1 = 0;
+    	int ptr2 = 1;
+    	
+    	while(ptr2 < arr.length) {
+    		if((arr[ptr1]!= 0 && arr[ptr2]!= 0) || (arr[ptr1] != 0 && arr[ptr2] == 0)) {
+    			ptr1 += 1;
+    			ptr2 += 1;
+    			
+    		}
+    		else if(arr[ptr1]== 0 && arr[ptr2]!=0) {
+    			arr[ptr1] = arr[ptr2];
+    			arr[ptr2] = 0;
+    			ptr1 += 1;
+    			ptr2 += 1;
+    		}
+    		else if(arr[ptr1] == 0 && arr[ptr2] == 0) {
+    			ptr2 += 1;
+    		}
+    	
+    	}
+    }
+}

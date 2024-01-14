078502315ff77298987066196a278d5790dde993
Balaji <60376123+Balaji0112@users.noreply.github.com>
2024-01-14 13:32:34 -0800

    Time: 11 ms (7.74%), Space: 41.9 MB (25.03%) - LeetHub

--- 1704-determine-if-string-halves-are-alike/1704-determine-if-string-halves-are-alike.java
+++ 1704-determine-if-string-halves-are-alike/1704-determine-if-string-halves-are-alike.java
@@ -1,7 +1,6 @@
 class Solution {
     public boolean halvesAreAlike(String s) {
                 int count1 = 0;
-                int count2 = 0;
 
                 for(int i = 0; i< (s.length()/2); i++) {		
                     if(s.charAt(i) == 'a' || s.charAt(i) == 'e' || s.charAt(i) == 'i' || s.charAt(i) == 'o' || s.charAt(i) == 'u' 
@@ -13,12 +12,12 @@ class Solution {
                 for(int i = (s.length()/2); i<s.length(); i++ ) {
                     if(s.charAt(i) == 'a' || s.charAt(i) == 'e' || s.charAt(i) == 'i' || s.charAt(i) == 'o' || s.charAt(i) == 'u' 
                             || s.charAt(i) == 'A' || s.charAt(i) == 'E' || s.charAt(i) == 'I' || s.charAt(i) == 'O' || s.charAt(i) == 'U') {
-                        count2++;
+                        count1--;
                     }
                 }
 
 
-                if(count1 == count2) {
+                if(count1 == 0) {
                     return true;
                 }
 

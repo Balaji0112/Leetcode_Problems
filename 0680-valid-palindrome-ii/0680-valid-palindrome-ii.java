7767fb894943db9a4d55ad972b5e723ce030c2d3
Balaji <60376123+Balaji0112@users.noreply.github.com>
2025-09-22 00:40:45 -0700

    Time: 74 ms (5.07%), Space: 45.4 MB (62.16%) - LeetHub

---
+++ 0680-valid-palindrome-ii/0680-valid-palindrome-ii.java
@@ -0,0 +1,35 @@
+class Solution {
+
+    private boolean isPalindrome(String s, int left, int right){
+        while(left < right){
+            if(s.charAt(left) == s.charAt(right)){
+                left++;
+                right--;
+            }
+            else{
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    public boolean validPalindrome(String s) {
+        s = s.replaceAll("[^a-zA-Z0-9]","");
+        int startPtr = 0;
+        int endPtr = s.length()-1;
+        int c = 1;
+        while(startPtr < endPtr){
+            if(s.charAt(startPtr) == s.charAt(endPtr)){
+                startPtr++;
+                endPtr--;
+                continue;
+            }
+            else{
+                return isPalindrome(s, startPtr+1,endPtr) || isPalindrome(s, startPtr, endPtr-1);
+            }
+        }
+
+        return true;
+    }
+}

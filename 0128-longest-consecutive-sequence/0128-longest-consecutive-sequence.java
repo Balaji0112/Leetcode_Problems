560cd48d3eea90a3bde0415bb2757b799718763a
Balaji <60376123+Balaji0112@users.noreply.github.com>
2025-09-25 21:35:12 -0700

    Time: 58 ms (7.95%), Space: 60.1 MB (71.21%) - LeetHub

--- 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.java
+++ 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.java
@@ -1,42 +1,31 @@
 class Solution {
     public int longestConsecutive(int[] nums) {
+        Map <Integer, Boolean> visitedMap = new HashMap<>();
+        int longestLength = 0;
+        for(int num: nums){
+            visitedMap.put(num, Boolean.FALSE);
+        }
 
-    int longestLength = 0;
-    Map<Integer, Boolean> numberTravelledMap = new HashMap<>();
-    for (int num : nums) {
-      numberTravelledMap.put(num, Boolean.FALSE);
-    }
+        for(int num: nums){
+            int currLength = 1;
 
-    for (int num : nums) {
-      int currentLength = 1;
+            int nextNum = num + 1;
+            while(visitedMap.containsKey(nextNum) && visitedMap.get(nextNum) == false){
+                currLength++;
+                visitedMap.put(nextNum, Boolean.TRUE);
+                nextNum++;
+            }
 
-      // Check in forward direction
-      int nextNum = num + 1;
-      while (numberTravelledMap.containsKey(nextNum) &&
-          numberTravelledMap.get(nextNum) == false) {
+            int prevNum = num - 1;
+            while(visitedMap.containsKey(prevNum) && !visitedMap.get(prevNum)){
+                currLength++;
+                visitedMap.put(prevNum, Boolean.TRUE);
+                prevNum--;
+            }
 
-        currentLength++;
-        numberTravelledMap.put(nextNum, Boolean.TRUE);
+            longestLength = Math.max(longestLength, currLength);
+        }
 
-        // Move to the next number
-        nextNum++;
-      }
-
-      // Check in reverse direction
-      int prevNum = num - 1;
-      while (numberTravelledMap.containsKey(prevNum) &&
-          numberTravelledMap.get(prevNum) == false) {
-
-        currentLength++;
-        numberTravelledMap.put(prevNum, Boolean.TRUE);
-
-        // Move to the previous number
-        prevNum--;
-      }
-
-      longestLength = Math.max(longestLength, currentLength);
-    }
-
-    return longestLength;
+        return longestLength;
     }
 }
